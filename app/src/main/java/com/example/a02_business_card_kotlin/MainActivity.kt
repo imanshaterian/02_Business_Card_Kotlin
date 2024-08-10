92235c322daca7ffcd4b9a071d5ed6da368faef2
Iman Shaterian <imanshaterian@gmail.com>
2024-08-10 11:46:05 +0200

    Initial commit for business card. created the foundation for the second project.

---
+++ app/src/main/java/com/example/a02_business_card_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a02_business_card_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a02_business_card_kotlin.ui.theme._02_Business_Card_KotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _02_Business_Card_KotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _02_Business_Card_KotlinTheme {
+        Greeting("Android")
+    }
+}
